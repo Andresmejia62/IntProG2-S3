1cfaf6818c4d7fa3c488abadba3df13c66119203
Andrés Mejía <amejia@uamv.edu.ni>
2025-04-01 11:33:10 -0600

    Update ejercicio2.py

--- ejercicio2.py
+++ ejercicio2.py
@@ -2,6 +2,6 @@ base_rect = float(input("Introduce la base del rectángulo: "))
 alt_rect = float(input("Introduce la altura del rectángulo: "))
 area_rect = base_rect * alt_rect
 perimetro_rect = (2 * base_rect) + (2 * alt_rect)
-perimetro_rect = perimetro_rect + 2
+
 print(f"El área del rectángulo es {area_rect:.2f}")
 print(f"El perímetro del rectángulo es {perimetro_rect:.2f}")
